9debfa5b6848207b60b3e2f5d829fb4b834c7213
Cedric <cedric@swivvel.io>
2023-09-12 10:13:44 -0400

    temp

--- src/background/configureDeepLinking.ts
+++ src/background/configureDeepLinking.ts
@@ -1,7 +1,10 @@
 import path from 'path';
 
 import { app, dialog } from 'electron';
+import electronAppUniversalProtocolClient from 'electron-app-universal-protocol-client';
 import log from 'electron-log';
+
+import { isProduction } from './utils';
 
 /**
  * Configure URL protocol used for deep linking to the desktop app.
@@ -9,40 +12,53 @@ import log from 'electron-log';
 export default async (): Promise<void> => {
   log.info(`Configuring deep linking...`);
 
-  if (process.defaultApp) {
-    log.info(`  process.defaultApp=true`);
-    if (process.argv.length >= 2) {
-      log.info(`  Using argument ${process.argv[1]}`);
-      app.setAsDefaultProtocolClient(`swivvel`, process.execPath, [
-        path.resolve(process.argv[1]),
-      ]);
-    }
-  } else {
-    log.info(`  process.defaultApp=false`);
-    app.setAsDefaultProtocolClient(`swivvel`);
-  }
+  electronAppUniversalProtocolClient.on(`request`, (url) => {
+    console.log(`electronAppUniversalProtocolClient`);
+    console.log(url);
+    log.info(`electronAppUniversalProtocolClient`);
+    log.info(url);
+    dialog.showErrorBox(`Welcome Back`, `You arrived from: ${url}`);
+  });
 
-  const gotTheLock = app.requestSingleInstanceLock();
+  await electronAppUniversalProtocolClient.initialize({
+    protocol: `swivvel`,
+    mode: isProduction() ? `production` : `development`,
+  });
 
-  if (!gotTheLock) {
-    app.quit();
-  } else {
-    app.on(`second-instance`, (event, commandLine, workingDirectory) => {
-      console.log(`!!!!!!!!!! second-instance`);
-      console.log(event);
-      console.log(commandLine);
-      console.log(workingDirectory);
-      dialog.showErrorBox(
-        `Welcome Back`,
-        `You arrived from: ${commandLine?.pop()?.slice(0, -1)}`
-      );
-    });
+  // if (process.defaultApp) {
+  //   log.info(`  process.defaultApp=true`);
+  //   if (process.argv.length >= 2) {
+  //     log.info(`  Using argument ${process.argv[1]}`);
+  //     app.setAsDefaultProtocolClient(`swivvel`, process.execPath, [
+  //       path.resolve(process.argv[1]),
+  //     ]);
+  //   }
+  // } else {
+  //   log.info(`  process.defaultApp=false`);
+  //   app.setAsDefaultProtocolClient(`swivvel`);
+  // }
 
-    // Handle the protocol. In this case, we choose to show an Error Box.
-    app.on(`open-url`, (event, url) => {
-      dialog.showErrorBox(`Welcome Back`, `You arrived from: ${url}`);
-    });
-  }
+  // const gotTheLock = app.requestSingleInstanceLock();
+
+  // if (!gotTheLock) {
+  //   app.quit();
+  // } else {
+  //   app.on(`second-instance`, (event, commandLine, workingDirectory) => {
+  //     console.log(`!!!!!!!!!! second-instance`);
+  //     console.log(event);
+  //     console.log(commandLine);
+  //     console.log(workingDirectory);
+  //     dialog.showErrorBox(
+  //       `Welcome Back`,
+  //       `You arrived from: ${commandLine?.pop()?.slice(0, -1)}`
+  //     );
+  //   });
+
+  //   // Handle the protocol. In this case, we choose to show an Error Box.
+  //   app.on(`open-url`, (event, url) => {
+  //     dialog.showErrorBox(`Welcome Back`, `You arrived from: ${url}`);
+  //   });
+  // }
 
   log.info(`Configured deep linking`);
 };
