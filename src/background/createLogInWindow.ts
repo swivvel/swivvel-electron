ad5a03d48d0b9309b5f4824f7ef0c34b00d9dd8c
Cedric <cedric@swivvel.io>
2023-09-12 14:03:51 -0400

    temp

--- src/background/createLogInWindow.ts
+++ src/background/createLogInWindow.ts
@@ -1,20 +1,12 @@
 import { BrowserWindow } from 'electron';
 import log from 'electron-log';
 
-import { isLinux, isProduction, sleep } from './utils';
-
 export default async (preloadPath: string): Promise<BrowserWindow> => {
   log.info(`Creating log in window...`);
-
-  // Transparent windows don't work on Linux without some hacks
-  // like this short delay
-  // See: https://github.com/electron/electron/issues/15947
-  if (isLinux()) {
-    await sleep(1000);
-  }
+  log.info(preloadPath);
 
   const logInWindow = new BrowserWindow({
-    height: 650,
+    height: 700,
     webPreferences: { preload: preloadPath },
     width: 720,
   });
@@ -23,9 +15,7 @@ export default async (preloadPath: string): Promise<BrowserWindow> => {
 
   await logInWindow.loadURL(`/`);
 
-  if (!isProduction()) {
-    logInWindow.webContents.openDevTools();
-  }
+  logInWindow.webContents.openDevTools();
 
   log.info(`Created log in window`);
 
