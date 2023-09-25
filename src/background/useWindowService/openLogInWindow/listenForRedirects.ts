3275e13b5c4fd44998ec446cbf795550b19bf7d6
Cedric <119253005+cedric-swivvel@users.noreply.github.com>
2023-09-25 06:43:32 -0400

    Fix Google browser sign in with Auth0 custom domain (#9)
    ## The Problem
    
    We updated Auth0 to use a custom domain, but the Electron app is still
    listening for redirects to the old Auth0 domain, so the app is no longer
    redirecting users to their browser for the Google sign in flow.
    
    ## The Solution
    
    Add the new custom Auth0 domain to the list of domains that we detect
    when determining whether a redirect should be opened in the browser. I
    left the old domain in there for now in case anyone is running old web
    code.

--- src/background/useWindowService/openLogInWindow/listenForRedirects.ts
+++ src/background/useWindowService/openLogInWindow/listenForRedirects.ts
@@ -16,7 +16,10 @@ export default (
     const { url } = event;
     log.info(`Caught redirect in log in window: ${removeQueryParams(url)}`);
 
-    if (url.includes(`auth0.com/authorize`)) {
+    if (
+      url.includes(`auth0.com/authorize`) ||
+      url.includes(`auth.swivvel.io`)
+    ) {
       // The `swivvel://` protocol doesn't work in development environments,
       // so we have to perform the log in flow within Electron
       if (!isProduction()) {
