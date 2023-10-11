a375ef619c69497c43557c6a61ca2428d37de8a6
Cedric <119253005+cedric-swivvel@users.noreply.github.com>
2023-10-11 14:36:12 -0400

    Hide transparent window in screenshots (#28)
    ## The Problem
    
    The Swivvel audio room widget is always on top of all windows and can be
    minimized but not closed completely. This makes it hard to take
    screenshots without the widget in them.
    
    ## The Solution
    
    Use the `window.setContentProtection(true)` command to prevent the
    transparent window from appearing in screenshots. According to the
    [Electron
    docs](https://www.electronjs.org/docs/latest/api/browser-window#winsetcontentprotectionenable-macos-windows):
    
    > Prevents the window contents from being captured by other apps.
    >
    > On macOS it sets the NSWindow's sharingType to NSWindowSharingNone. On
    Windows it calls SetWindowDisplayAffinity with WDA_EXCLUDEFROMCAPTURE.
    For Windows 10 version 2004 and up the window will be removed from
    capture entirely, older Windows versions behave as if WDA_MONITOR is
    applied capturing a black window.
    
    ## Other Changes
    
    - Add a PR template to ensure consistency in PR titles and descriptions
    - Add a GitHub action to enforce PR title length

--- src/background/useWindowService/openTransparentWindow/openTransparentWindow.ts
+++ src/background/useWindowService/openTransparentWindow/openTransparentWindow.ts
@@ -14,6 +14,10 @@ const openTransparentWindow: OpenTransparentWindow = async (args) => {
   const options = getTransparentBrowserWindowOptions();
 
   return openBrowserWindow(state, `transparent`, options, async (window) => {
+    // Prevent the transparent window from appearing in screenshots
+    // See: https://www.electronjs.org/docs/latest/api/browser-window#winsetcontentprotectionenable-macos-windows
+    window.setContentProtection(true);
+
     // Show the window but don't focus it because it would be confusing to users
     // if an invisible window took focus.
     window.showInactive();
