1b3414d2535e8ff78f544c16aa24e0403a33fed8
Cedric <cedric@swivvel.io>
2023-10-11 13:56:30 -0400

    temp

--- src/background/useWindowService/openHqWindow/getHqWindowBrowserOptions.ts
+++ src/background/useWindowService/openHqWindow/getHqWindowBrowserOptions.ts
@@ -1,15 +1,30 @@
 import { BrowserWindowConstructorOptions, screen } from 'electron';
 
-import { getPreloadPath } from '../../utils';
+import { getPreloadPath, isLinux } from '../../utils';
 
 export default (show: boolean): BrowserWindowConstructorOptions => {
   const primaryDisplay = screen.getPrimaryDisplay();
 
+  let height: number;
+  let width: number;
+
+  if (isLinux()) {
+    // There's a bug on Ubuntu where the window title bar is transparent if
+    // the window is sized to take up the entire work area, so make the window
+    // large enough that it will probably fit on most screens.
+    height = Math.min(primaryDisplay.workArea.height, 1000);
+    width = Math.min(primaryDisplay.workArea.width, 1400);
+  } else {
+    height = primaryDisplay.workArea.height;
+    width = primaryDisplay.workArea.width;
+  }
+
   return {
     backgroundColor: `#ffffff`,
-    height: Math.min(primaryDisplay.workArea.height, 800),
+    center: true,
+    height,
     show,
     webPreferences: { preload: getPreloadPath() },
-    width: Math.min(primaryDisplay.workArea.width, 1400),
+    width,
   };
 };
