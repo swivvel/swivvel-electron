swivvel/swivvel-electron

commit 571dd85205a99f3f03eff18b8eec872463b9cf47

Report errors to Sentry; add manual error reporting tray item (#31)
## The Problem

We don't have a lot of visibility into errors that users encounter in
the Electron app. Right now our debugging process requires us to ask the
user to explain the issue and manually send us the console logs and the
Electron log file. This makes it difficult to debug issues.

## The Solution

Add automatic Sentry error reporting, as well as a new "Send Bug Report"
item in the tray menu that will attach the Electron `main.log` file with
the Sentry alert.

## Other Changes

Change `npm run dev` to `electron .` because the development environment
wasn't detecting our `package.json` so it was using the app name
`Electron` instead of `Swivvel`. This was causing log files to be
written to an `Electron-development/` directory.

src/background/background.ts
@@ -5,16 +5,26 @@ import configureApp from './configureApp';
 import configureAppQuitHandling from './configureAppQuitHandling';
 import configureAutoUpdates from './configureAutoUpdates';
 import configureIpcHandlers from './configureIpcHandlers';
+import configureSentry from './configureSentry';
 import getDeepLinkHandler from './getDeepLinkHandler';
 import handlePowerMonitorStateChanges from './handlePowerMonitorStateChanges';
 import listenForDeepLinks from './listenForDeepLinks';
 import pollForIdleTime from './pollForIdleTime';
+import setUserDataPath from './setUserDataPath';
 import { State } from './types';
 import useTrayService from './useTrayService';
 import useWindowService from './useWindowService';
 
+// Call as early as possible since this changes the directory path where
+// Electron stores everything related to the app
+setUserDataPath();
+
 const run = async (): Promise<void> => {
   log.info(`App v=${app.getVersion()} starting...`);
+  log.info(`User Data: ${app.getPath(`userData`)}`);
+
+  // Call at the beginning so that all exceptions are captured
+  configureSentry();
 
   const state: State = {
     allowQuit: false,

src/background/setUserDataPath.ts
@@ -1,0 +1,21 @@
+import path from 'path';
+
+import { app } from 'electron';
+import log from 'electron-log';
+
+import { isProduction } from './utils';
+
+export default (): void => {
+  if (!isProduction()) {
+    // Electron stores everything related to the app (cache, local storage,
+    // databases, logs, etc.) in a directory in the user's home directory.
+    // Prevent dev from colliding with prod by adding a suffix.
+    const userDataPath = `${app.getPath(`userData`)}-development`;
+    app.setPath(`userData`, userDataPath);
+
+    // Make sure the main logger writes to the new user data path
+    log.transports.file.resolvePath = (): string => {
+      return path.join(userDataPath, `logs`, `main.log`);
+    };
+  }
+};

src/background/useWindowService/openTransparentWindow/getLogger.ts
@@ -1,0 +1,23 @@
+import path from 'path';
+
+import { app } from 'electron';
+import log from 'electron-log';
+
+let globalLogger: log.ElectronLog | null = null;
+
+/**
+ * Get a logger that writes to a different file than the main logger.
+ */
+export default (name: string): log.ElectronLog => {
+  if (!globalLogger) {
+    log.info(`Initializing logger: ${name}`);
+
+    globalLogger = log.create(name);
+    globalLogger.transports.console.level = false;
+    globalLogger.transports.file.resolvePath = (): string => {
+      return path.join(app.getPath(`userData`), `logs`, `${name}.log`);
+    };
+  }
+
+  return globalLogger;
+};

src/background/configureApp.ts
@@ -1,7 +1,7 @@
 import { app } from 'electron';
 import log from 'electron-log';
 
-import { isLinux, isProduction } from './utils';
+import { isLinux } from './utils';
 
 /**
  * Configure the global app settings.
@@ -9,18 +9,11 @@ import { isLinux, isProduction } from './utils';
 export default (): void => {
   log.info(`Configuring app...`);
 
-  // Electron stores everything related to the app (cache, local storage,
-  // databases, logs, etc.) in a directory in the user's home directory.
-  // Prevent dev from colliding with prod by adding a suffix.
-  if (!isProduction()) {
-    app.setPath(`userData`, `${app.getPath(`userData`)}-development`);
-  }
-
-  log.info(`User Data: ${app.getPath(`userData`)}`);
-
-  // Transparent windows don't work in Linux without these settings
-  // See: https://github.com/electron/electron/issues/15947
   if (isLinux()) {
+    log.info(`Setting Linux configuration options...`);
+
+    // Transparent windows don't work in Linux without these settings
+    // See: https://github.com/electron/electron/issues/15947
     app.commandLine.appendSwitch(`enable-transparent-visuals`);
     app.commandLine.appendSwitch(`disable-gpu`);
     app.disableHardwareAcceleration();

src/background/configureIpcHandlers.ts
@@ -1,3 +1,4 @@
+import * as Sentry from '@sentry/electron/main';
 import { app, ipcMain } from 'electron';
 import log from 'electron-log';
 
@@ -10,6 +11,26 @@ export default (windowService: WindowService): void => {
   ipcMain.handle(`getDesktopAppVersion`, () => {
     return app.getVersion();
   });
+
+  ipcMain.handle(
+    `getDesktopSources`,
+    async (): Promise<Array<ShareableMediaSource>> => {
+      log.info(`Received getDesktopSources event`);
+      return getShareableMediaSources();
+    }
+  );
+
+  ipcMain.on(
+    `identifyUser`,
+    async (
+      event,
+      user: { id: string; email: string | null } | null
+    ): Promise<void> => {
+      log.info(`Received identifyUser event, user=${JSON.stringify(user)}`);
+      log.info(`Setting Sentry user context`);
+      Sentry.setUser(user ? { ...user, email: user.email || undefined } : null);
+    }
+  );
 
   ipcMain.handle(`isProduction`, isProduction);
 
@@ -33,13 +54,4 @@ export default (windowService: WindowService): void => {
     log.info(`Sending launchAudioRoomFromSetup event to transparent window`);
     transparentWindow.webContents.send(`launchAudioRoomFromSetup`);
   });
-
-  ipcMain.handle(
-    `getDesktopSources`,
-    async (): Promise<Array<ShareableMediaSource>> => {
-      log.info(`Received getDesktopSources event`);
-
-      return getShareableMediaSources();
-    }
-  );
 };

src/background/configureSentry.ts
@@ -1,0 +1,45 @@
+import path from 'path';
+
+import * as Sentry from '@sentry/electron/main';
+import log from 'electron-log';
+
+import { isProduction } from './utils';
+
+export default (): void => {
+  Sentry.init({
+    dsn: isProduction()
+      ? `https://2dc312dd96e84f3ca06fa0e5e3f96c5e@o4504796289433600.ingest.sentry.io/4504796469657600`
+      : `https://c63ffcba511c4cdebd5365c4cb2990f6@o4504796289433600.ingest.sentry.io/4504796459171840`,
+  });
+
+  Sentry.setTag(`swivvel.service`, `electron`);
+
+  // Attach the log files to every Sentry alert
+  Sentry.addGlobalEventProcessor((event, hint) => {
+    log.info(`Sentry addGlobalEventProcessor called`);
+
+    // It is important for the Sentry alert to still get reported even if
+    // we fail to attach the log files
+    try {
+      log.info(`Reading all log files...`);
+
+      const allLogs = log.transports.file.readAllLogs();
+      const paths = allLogs.map((logFile) => {
+        return logFile.path;
+      });
+
+      log.info(`Attaching log files: ${JSON.stringify(paths)}`);
+
+      hint.attachments = allLogs.map((logFile) => {
+        return {
+          filename: path.basename(logFile.path),
+          data: logFile.lines.join(`\n`),
+        };
+      });
+    } catch (err) {
+      log.error(`Error reading log files: ${err}`);
+    }
+
+    return event;
+  });
+};

src/background/useTrayService/utils/getBaseMenuItems.ts
@@ -1,4 +1,6 @@
-import { MenuItemConstructorOptions } from 'electron';
+import * as Sentry from '@sentry/electron/main';
+import { MenuItemConstructorOptions, dialog } from 'electron';
+import log from 'electron-log';
 
 import { State } from '../../types';
 import { isLinux, isProduction, quitApp } from '../../utils';
@@ -21,6 +23,21 @@ export default (state: State): Array<MenuItemConstructorOptions> => {
       },
     });
   }
+
+  menuItems.push({
+    label: `Send Bug Report`,
+    type: `normal`,
+    click: (): void => {
+      log.info(
+        `Detected click on Send Bug Report menu item; sending Sentry alert`
+      );
+      Sentry.captureException(new Error(`Manual bug report`));
+      dialog.showErrorBox(
+        `Bug report submitted`,
+        `Oh snap! Sorry about that. We'll look into this. Please send us a brief description and screenshot via your company's Slack Connect channel or email us at support@swivvel.io. We'll get back to you ASAP.`
+      );
+    },
+  });
 
   menuItems.push({
     label: `Quit Swivvel`,

src/background/useWindowService/openTransparentWindow/openTransparentWindow.ts
@@ -2,6 +2,7 @@ import { getSiteUrl, isLinux, loadUrl, sleep } from '../../utils';
 import { openBrowserWindow } from '../utils';
 
 import configureCloseHandler from './configureCloseHandler';
+import getLogger from './getLogger';
 import getTransparentBrowserWindowOptions from './getTransparentBrowserWindowOptions';
 import pollForMouseEvents from './pollForMouseEvents';
 import resizeOnDisplayChange from './resizeOnDisplayChange';
@@ -11,6 +12,7 @@ import { OpenTransparentWindow } from './types';
 const openTransparentWindow: OpenTransparentWindow = async (args) => {
   const { state, windowOpenRequestHandler } = args;
 
+  const transparentWindowLog = getLogger(`transparent`);
   const options = getTransparentBrowserWindowOptions();
 
   return openBrowserWindow(state, `transparent`, options, async (window) => {
@@ -26,14 +28,20 @@ const openTransparentWindow: OpenTransparentWindow = async (args) => {
     // on top, so we have to explicitly re-enable the always-on-top setting.
     window.setAlwaysOnTop(true);
 
+    // Write all console messages to a log file so that we can include the file
+    // in Sentry alerts.
+    window.webContents.on(`console-message`, (event, level, message) => {
+      transparentWindowLog.info(message);
+    });
+
+    window.webContents.setWindowOpenHandler(windowOpenRequestHandler);
+
     // Transparent windows don't work on Linux without some hacks
     // like this short delay
     // See: https://github.com/electron/electron/issues/15947
     if (isLinux()) {
       await sleep(1000);
     }
-
-    window.webContents.setWindowOpenHandler(windowOpenRequestHandler);
 
     showOnAllWorkspaces(window);
     configureCloseHandler(window, state);

commit 7e4c20920f40b7204ace8c9505e87e66e56d3056

Write transparent window logs & dev logs to correct directory (#37)
## The Problem

In commit 571dd85205a99f3f03eff18b8eec872463b9cf47, we made two changes
related to logging:

1. We started writing logs from the transparent window into a file.
2. We fixed a bug where app data was stored in a directory called
"Electron" instead of "Swivvel-development". Fixing this bug required us
to explicitly configure the development logs to be written into the new
"Swivvel-development" directory.

Both of these changes configured the log path using code like this:

```ts
path.join(app.getPath(`userData`), `logs`, `${name}.log`);
```

This worked on Windows and Linux because the log data is stored in a
`logs/` sub-directory inside of the Electron user data directory.

However, on Mac the log files are _not_ stored in the user data
directory. The user data is stored in `~/Library/Application
Support/Swivvel/` whereas the logs are stored in
`~/Library/Logs/Swivvel/`. The changes in commit
571dd85205a99f3f03eff18b8eec872463b9cf47 caused the logs to incorrectly
be written to `~/Library/Application Support/Swivvel/logs`.

## The Solution

When configuring the log path, always use `app.getPath('logs')` as the
base, not `app.getPath('userData')`.

src/background/background.ts
@@ -22,6 +22,7 @@ setUserDataPath();
 const run = async (): Promise<void> => {
   log.info(`App v=${app.getVersion()} starting...`);
   log.info(`User Data: ${app.getPath(`userData`)}`);
+  log.info(`Logs: ${app.getPath(`logs`)}`);
 
   // Call at the beginning so that all exceptions are captured
   configureSentry();

src/background/setUserDataPath.ts
@@ -7,15 +7,21 @@ import { isProduction } from './utils';
 
 export default (): void => {
   if (!isProduction()) {
-    // Electron stores everything related to the app (cache, local storage,
-    // databases, logs, etc.) in a directory in the user's home directory.
-    // Prevent dev from colliding with prod by adding a suffix.
-    const userDataPath = `${app.getPath(`userData`)}-development`;
-    app.setPath(`userData`, userDataPath);
+    const userDataProd = app.getPath(`userData`);
+    const logsProd = app.getPath(`logs`);
 
-    // Make sure the main logger writes to the new user data path
+    // The paths that Electron uses for storing app data are derived from
+    // the app name, Swivvel. To prevent development data from colliding
+    // with prod data, we add a suffix.
+    const userDataDev = userDataProd.replace(`Swivvel`, `Swivvel-development`);
+    const logsDev = logsProd.replace(`Swivvel`, `Swivvel-development`);
+
+    app.setPath(`userData`, userDataDev);
+    app.setPath(`logs`, logsDev);
+
+    // Make sure the main logger writes to the new log path
     log.transports.file.resolvePath = (): string => {
-      return path.join(userDataPath, `logs`, `main.log`);
+      return path.join(logsDev, `main.log`);
     };
   }
 };

src/background/useWindowService/openTransparentWindow/getLogger.ts
@@ -10,12 +10,14 @@ let globalLogger: log.ElectronLog | null = null;
  */
 export default (name: string): log.ElectronLog => {
   if (!globalLogger) {
-    log.info(`Initializing logger: ${name}`);
+    const logPath = path.join(app.getPath(`logs`), `${name}.log`);
+
+    log.info(`Initializing logger '${name}' at path ${logPath}`);
 
     globalLogger = log.create(name);
     globalLogger.transports.console.level = false;
     globalLogger.transports.file.resolvePath = (): string => {
-      return path.join(app.getPath(`userData`), `logs`, `${name}.log`);
+      return logPath;
     };
   }
 