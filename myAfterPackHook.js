9c50ad8ae8ce35329830c914cf2fdd44ea8458dc
Eric Ropiak <eric@swivvel.io>
2023-10-16 11:01:35 -0400

    maybe

--- myAfterPackHook.js
+++ myAfterPackHook.js
@@ -14,14 +14,14 @@ exports.default = async (configuration) => {
   console.log(execSync(`ls dist/win-unpacked`).toString());
 
   execSync(
-    `signtool.exe sign /sha1 ${hash} /tr http://timestamp.digicert.com /td SHA256 /fd SHA256 "./dist/win-unpacked/Swivvel-win.exe"`
+    `signtool.exe sign /sha1 ${hash} /tr http://timestamp.digicert.com /td SHA256 /fd SHA256 "./dist/win-unpacked/Swivvel.exe"`
   );
-  execSync(`signtool.exe verify /v /pa "./dist/win-unpacked/Swivvel-win.exe"`);
+  execSync(`signtool.exe verify /v /pa "./dist/win-unpacked/Swivvel.exe"`);
 
-  execSync(
-    `signtool.exe sign /sha1 ${hash} /tr http://timestamp.digicert.com /td SHA256 /fd SHA256 "D:\\a\\swivvel-electron\\swivvel-electron\\dist\\Swivvel-win.exe"`
-  );
-  execSync(
-    `signtool.exe verify /v /pa "D:\\a\\swivvel-electron\\swivvel-electron\\dist\\Swivvel-win.exe"`
-  );
+  // execSync(
+  //   `signtool.exe sign /sha1 ${hash} /tr http://timestamp.digicert.com /td SHA256 /fd SHA256 "D:\\a\\swivvel-electron\\swivvel-electron\\dist\\Swivvel-win.exe"`
+  // );
+  // execSync(
+  //   `signtool.exe verify /v /pa "D:\\a\\swivvel-electron\\swivvel-electron\\dist\\Swivvel-win.exe"`
+  // );
 };
