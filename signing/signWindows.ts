a9807b4ea22179357ad733340ca566acc555f7e0
Eric Ropiak <eric@swivvel.io>
2023-10-16 12:47:59 -0400

    untracked

---
+++ signing/signWindows.ts
@@ -0,0 +1,23 @@
+import { execSync } from 'child_process';
+
+import { CustomWindowsSignTaskConfiguration } from 'electron-builder';
+
+export default async (
+  configuration: CustomWindowsSignTaskConfiguration
+): Promise<void> => {
+  const getEnv = (name: string): string | null => {
+    return process.env[name.toUpperCase()] || null;
+  };
+
+  const signingHash = getEnv(`SM_CODE_SIGNING_CERT_SHA1_HASH`);
+
+  const file = configuration.path;
+
+  const signCmd = `signtool.exe sign /sha1 ${signingHash} /tr http://timestamp.digicert.com /td SHA256 /fd SHA256 "${file}"`;
+  // eslint-disable-next-line no-console
+  console.log(execSync(signCmd).toString());
+
+  const verifyCmd = `signtool.exe verify /v /pa "${file}"`;
+  // eslint-disable-next-line no-console
+  console.log(execSync(verifyCmd).toString());
+};
