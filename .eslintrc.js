ba178ede870c44450b850ec1220a3278cb6f765e
Eric Ropiak <eric@swivvel.io>
2023-10-16 15:04:11 -0400

    lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  ignorePatterns: [`.eslintrc.js`, `compiled/`, `dist/`],
+  ignorePatterns: [`.eslintrc.js`, `compiled/`, `dist/`, `myAfterPackHook.js`],
   overrides: [
     {
       files: `*.{js,ts}`,
