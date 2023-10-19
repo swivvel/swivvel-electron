4b6790935b8e5b04a45a654423894571d75330b6
Cedric <cedric@swivvel.io>
2023-10-19 07:33:44 -0400

    temp

--- src/background/useWindowService/openGoogleMeetWindow/scrapeAndSaveMeetingUrl.ts
+++ src/background/useWindowService/openGoogleMeetWindow/scrapeAndSaveMeetingUrl.ts
@@ -12,18 +12,26 @@ export default (window: BrowserWindow, log: Log): Promise<string> => {
         return;
       }
 
-      const rawMeetingUrl = await window.webContents.executeJavaScript(
-        `document.querySelector('[data-meeting-link]').dataset.meetingLink`
-      );
+      let rawMeetingUrl: unknown;
 
-      if (rawMeetingUrl) {
+      // Sometimes the user is prompted to sign in to their Google account
+      // before the `/getalink` page loads. If this happens, the document
+      // will not containing the meeting link and the JS will throw an error.
+      try {
+        rawMeetingUrl = await window.webContents.executeJavaScript(
+          `document.querySelector('[data-meeting-link]').dataset.meetingLink`
+        );
+      } catch (err) {
+        rawMeetingUrl = null;
+      }
+
+      if (rawMeetingUrl && typeof rawMeetingUrl === `string`) {
         const meetingUrl = rawMeetingUrl.startsWith(`http`)
           ? rawMeetingUrl
           : `https://${rawMeetingUrl}`;
 
-        resolve(meetingUrl);
-
         clearInterval(interval);
+        resolve(meetingUrl);
       }
     }, 50);
   });
