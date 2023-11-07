3a9fab6dafaa3920ae138daace3910a93a6ff53a
Cedric <cedric@swivvel.io>
2023-11-07 13:30:29 -0500

    temp

--- src/background/useWindowService/openTransparentWindow/getFileLogger.ts
+++ src/background/useWindowService/openTransparentWindow/getFileLogger.ts
@@ -3,22 +3,37 @@ import path from 'path';
 import { app } from 'electron';
 import log from 'electron-log';
 
-let globalLogger: log.ElectronLog | null = null;
+const isFileTransport = (
+  transport: log.Transport
+): transport is log.FileTransport => {
+  return Boolean((transport as log.FileTransport).resolvePathFn);
+};
+
+let globalLogger: log.Logger | null = null;
 
 /**
  * Get a logger that writes to a different file than the main logger.
  */
-export default (name: string): log.ElectronLog => {
+export default (name: string): log.Logger => {
   if (!globalLogger) {
     const logPath = path.join(app.getPath(`logs`), `${name}.log`);
 
     log.info(`Initializing logger '${name}' at path ${logPath}`);
 
-    globalLogger = log.create(name);
-    globalLogger.transports.console.level = false;
-    globalLogger.transports.file.resolvePath = (): string => {
-      return logPath;
-    };
+    globalLogger = log.create({ logId: name });
+
+    if (globalLogger.transports.console) {
+      globalLogger.transports.console.level = false;
+    }
+
+    if (
+      globalLogger.transports.file &&
+      isFileTransport(globalLogger.transports.file)
+    ) {
+      globalLogger.transports.file.resolvePathFn = (): string => {
+        return logPath;
+      };
+    }
   }
 
   return globalLogger;
